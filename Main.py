8dedf0ddd2f3c338d885628d0512e5e2dd76157d
PunchPotato <119630204+PunchPotato@users.noreply.github.com>
2023-04-01 14:02:05 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import pygame
 import random
 from pygame.math import Vector2
+from a import Button
+import sys
 
-width = 1920
-height = 1080
+width = 1280
+height = 720
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -19,61 +21,148 @@ pygame.display.set_caption("AimTrainer")
 black = (0, 0, 0)
 blue = (0, 0, 128)
 pygame.font.get_fonts()
-font = pygame.font.SysFont("consolas", 40, bold=True)
 
 
-class CIRCLE:
-    def __init__(self):
-        self.pos = None
-        self.y = None
-        self.x = None
-        self.randomize()
-
-    def draw_circle(self):
-        global hit
-        global count_mouse_click
-        accuracy = 0
-        if hit and count_mouse_click >= 1:
-            accuracy = round(hit / count_mouse_click * 100, 1)
-        text = font.render(str(f"Your Accuracy Is: {accuracy}%"), True, black)
-        text_rect = text.get_rect()
-        text_rect.center = (width // 2, height // 15)
-        screen.blit(text, text_rect)
-
-        screen.blit(drawn_circle, (self.x, self.y))
-
-    def randomize(self):
-        self.x = random.randint(50, 1870)
-        self.y = random.randint(50, 1030)
-        self.pos = Vector2(self.x, self.y)
-
-    def check_click(self):
-        mouse_pos = pygame.mouse.get_pos()
-        global hit
-        global count_mouse_click
-        global drawn_circle
-        drawn_circle_rect = screen.blit(drawn_circle, (self.x, self.y))
-        left_click = pygame.mouse.get_pressed()[0]
-        circle_click = drawn_circle_rect
-        if left_click and circle_click.collidepoint(mouse_pos):
-            count_mouse_click += 1
-            hit += 1
-            self.__init__()
-        else:
-            count_mouse_click += 1
-            return False
+def get_font(size):
+    return pygame.font.SysFont("consolas", size, bold=True)
 
 
-circle = CIRCLE()
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                circle.check_click()
+def play():
+    class CIRCLE:
+        def __init__(self):
+            self.pos = None
+            self.y = None
+            self.x = None
+            self.randomize()
 
-    screen.blit(bg, (0, 0))
-    circle.draw_circle()
-    pygame.display.update()
-    clock.tick(120)
+        def draw_circle(self):
+            global hit
+            global count_mouse_click
+            accuracy = 0
+            if hit and count_mouse_click >= 1:
+                accuracy = round(hit / count_mouse_click * 100, 1)
+            text = get_font(45).render(str(f"Your Accuracy Is: {accuracy}%"), True, black)
+            text_rect = text.get_rect()
+            text_rect.center = (width // 2, height // 15)
+            screen.blit(text, text_rect)
+
+            screen.blit(drawn_circle, (self.x, self.y))
+
+        def randomize(self):
+            self.x = random.randint(50, 1230)
+            self.y = random.randint(50, 670)
+            self.pos = Vector2(self.x, self.y)
+
+        def check_click(self):
+            mouse_pos = pygame.mouse.get_pos()
+            global hit
+            global count_mouse_click
+            global drawn_circle
+            drawn_circle_rect = screen.blit(drawn_circle, (self.x, self.y))
+            left_click = pygame.mouse.get_pressed()[0]
+            circle_click = drawn_circle_rect
+            if left_click and circle_click.collidepoint(mouse_pos):
+                count_mouse_click += 1
+                hit += 1
+                self.__init__()
+            else:
+                count_mouse_click += 1
+                return False
+
+    circle = CIRCLE()
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        screen.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        screen.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    circle.check_click()
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        screen.blit(bg, (0, 0))
+        circle.draw_circle()
+        pygame.display.update()
+        clock.tick(120)
+
+
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        screen.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(200, 260))
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def main_menu():
+    while True:
+        screen.blit(bg, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("button/button sign but cool.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("button/button sign but cool.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("button/button sign but cool.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+main_menu()
